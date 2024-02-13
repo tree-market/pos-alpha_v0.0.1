d68f0439c29a11c995b9110d5654627a72ab730a
apollo5ever <backend@ambrosia.money>
2024-02-13 11:31:06 -0600

    fixed menu icon urls

--- src/components/sideMenu.tsx
+++ src/components/sideMenu.tsx
@@ -5,16 +5,14 @@ const SideMenu = ({toggleMenu,setView}:any) => {
     <div className="max-w-[90vh] main-container-menu absolute z-50 flex min-h-screen flex-col overflow-hidden bg-gray-50">
       <div className="menu-header relative grid grid-flow-col h-[64px] px-4">
         <div className="justify-self-start grid grid-flow-col items-center gap-1">
-          <div onClick={toggleMenu} className="menu-icon px-4 py-2 cursor-pointer justify-self-start text-xl font-semibold">
-            <img src="https://tree.market/img/icons/menu-open-icon.png" class="w-[28px]" />
-          </div>
+          <div onClick={toggleMenu} className="menu-icon px-4 py-2 cursor-pointer justify-self-start text-xl font-semibold">&#8806;</div>
           <div className="sitename justify-self-start">
             <div className="font-semibold text-xl">Tree.Market</div>
           </div>
         </div>
         <div className="connect-button grid justify-self-end items-center text-center">
           <div className="px-6 py-2 text-2xl">
-            <img src="https://tree.market/img/icons/treemarket-icon.png" class="w-[30px]" />
+            &#9752;
           </div>
         </div>
       </div>
